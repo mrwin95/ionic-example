6fb93199c1e522a1c971c17ecc994f223132ff57
Win Macbook <mrwin95@gmail.com>
2022-06-11 12:53:59 -0600

    login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,18 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./pages/dashboard/dashboard.module').then( m => m.DashboardPageModule)
+  },
 ];
 
 @NgModule({
---
+++ src/app/services/network.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Network } from '@capacitor/network';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NetworkService {
+
+
+  constructor() { }
+}
